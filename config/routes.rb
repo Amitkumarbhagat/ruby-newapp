75e6957c810ad44b76e161096c5f8d1449b3398e
Amit Bhagat <78423707+Amitkumarbhagat@users.noreply.github.com>
2023-05-06 02:21:05 +0530

    first commit for creating ror collage project

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+
+
+# config/routes.rb
+Rails.application.routes.draw do
+  root "static_pages#home"
+  post "sign_up", to: "users#create"
+  get "sign_up", to: "users#new"
+  resources :confirmations, only: [:create, :edit, :new], param: :confirmation_token
+end
