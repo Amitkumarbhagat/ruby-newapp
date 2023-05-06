39ae0ba3656e487ff8e29800d6b7a8dcf2e1e0c5
Amit Bhagat <78423707+Amitkumarbhagat@users.noreply.github.com>
2023-05-06 12:05:05 +0530

    create forms

--- config/routes.rb
+++ config/routes.rb
@@ -7,8 +7,13 @@ Rails.application.routes.draw do
   get "sign_up", to: "users#new"
   resources :confirmations, only: [:create, :edit, :new], param: :confirmation_token
 
+  put "account", to: "users#update"
+  get "account", to: "users#edit"
+  delete "account", to: "users#destroy"
+
   post "login", to: "sessions#create"
   delete "logout", to: "sessions#destroy"
   get "login", to: "sessions#new"
   resources :passwords, only: [:create, :edit, :new, :update], param: :password_reset_token
+
 end
